8713a7fc0a0dd69e1e4dc7fd928593c77122de4d
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-09 15:13:52 -0700

    it finds email addresses inside of text

--- email-address-detection/email_detect.rb
+++ email-address-detection/email_detect.rb
@@ -1,2 +1,10 @@
-class EmailDetect
+class EmailDetector
+  attr_reader :text
+  def initialize(text)
+    @text = [text]
+  end
+
+  def find_email
+    text[0].scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i).join
+  end
 end
--- email-address-detection/email_detect_test.rb
+++ email-address-detection/email_detect_test.rb
@@ -5,6 +5,19 @@ require './email_detect'
 
 class EmailDetectTest < Minitest::Test
   def test_it_exists
-    assert(EmailDetect)
+    assert EmailDetector.new("heyo")
+  end
+
+  def test_it_takes_and_stores_a_piece_of_text
+    email_detector = EmailDetector.new("Customer Care team is at your service (10:00am to 5:30pm) & will address your concerns immediately. You can also email us at: customerservice@tnmbonline.com
+NRI Help Desk:")
+    assert_equal "Customer Care team is at your service (10:00am to 5:30pm) & will address your concerns immediately. You can also email us at: customerservice@tnmbonline.com
+NRI Help Desk:", email_detector.text[0]
+  end
+
+  def test_it_finds_email_address_inside_text
+    email_detector = EmailDetector.new("Customer Care team is at your service (10:00am to 5:30pm) & will address your concerns immediately. You can also email us at: customerservice@tnmbonline.com
+NRI Help Desk:")
+    assert_equal "customerservice@tnmbonline.com", email_detector.find_email
   end
 end
